53d75615056ef48e7966ed385601131be9fc2f61
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-17 12:00:59 +0900

    文法エラーを修正

--- DryRun.bat
+++ DryRun.bat
@@ -8,13 +8,13 @@ setlocal
 
 IF "%~1" == ""  GOTO finish
 
-source=%~1
-output=%~n1.wmv
+set  source=%~1
+set  output=%~n1.wmv
 
 ECHO  Source : %source%
 ECHO  Output : %target%
 
-command_line=ffmpeg  -i %1 -c:v wmv2 -ac 2 -s 480x270 -b:a 320k -q:v 2 "%output%"
+set  command_line=ffmpeg  -i %1 -c:v wmv2 -ac 2 -s 480x270 -b:a 320k -q:v 2 "%output%"
 ECHO  RunCmd: %command_line%
 
 SHIFT
