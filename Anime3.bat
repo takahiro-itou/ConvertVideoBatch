d44b64a08edcd3ab1b7931055dbf5432879c3fce
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-17 12:49:18 +0900

    環境変数で追加のオプションを指定できるように修正

--- Anime3.bat
+++ Anime3.bat
@@ -13,9 +13,11 @@ set  output=%~n1.wmv
 
 ECHO  Source : %source%
 ECHO  Output : %output%
+ECHO  Option : %FFMPEG_OPTS%
 
 set  command_line=ffmpeg  -i "%source%"  ^
   -c:v wmv2 -ac 2 -s 480x270 -b:a 160k -r 15 -q:v 5  ^
+  %FFMPEG_OPTS%  ^
   "%output%"
 ECHO  RunCmd: %command_line%
 
