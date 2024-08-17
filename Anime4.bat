3b9d9f7cdf4822f31f938b83e72355913937a7aa
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-17 12:29:27 +0900

    フレームレートの指定忘れ

--- Anime4.bat
+++ Anime4.bat
@@ -15,7 +15,7 @@ ECHO  Source : %source%
 ECHO  Output : %output%
 
 set  command_line=ffmpeg  -i "%source%"  ^
-  -c:v wmv2 -ac 2 -s 480x270 -b:a 128k -q:v 7  ^
+  -c:v wmv2 -ac 2 -s 480x270 -b:a 160k -r 15 -q:v 9  ^
   "%output%"
 ECHO  RunCmd: %command_line%
 
