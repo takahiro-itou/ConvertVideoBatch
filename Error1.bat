76053a2f301ca7584e3913b24c617366f4947ff3
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-17 12:33:43 +0900

    バッチファイルを新規作成
    (1) 特番などで予定していた番組じゃなかった時用

---
+++ Error1.bat
@@ -0,0 +1,29 @@
+
+@ECHO OFF
+setlocal
+
+@REM  CD /D %~dp0
+
+:loop
+
+IF "%~1" == ""  GOTO finish
+
+set  source=%~1
+set  output=%~n1.wmv
+
+ECHO  Source : %source%
+ECHO  Output : %output%
+
+set  command_line=ffmpeg  -i "%source%"  ^
+  -c:v wmv2 -ac 2 -s 480x270 -b:a 64k -r 15 -q:v 25  ^
+  "%output%"
+ECHO  RunCmd: %command_line%
+
+@ECHO  ON
+%command_line%
+@ECHO  OFF
+
+SHIFT
+GOTO  loop
+
+:finish
