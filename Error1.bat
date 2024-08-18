fc192083749d976138b373a1d2a03bb51408cd4e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-18 19:46:55 +0900

    バッチファイルを更新
    (1) アスペクト比を指定
    (2) オーディオコーデックを明示
    (3) リファクタリング

--- Error1.bat
+++ Error1.bat
@@ -15,10 +15,14 @@ ECHO  Source : %source%
 ECHO  Output : %output%
 ECHO  Option : %FFMPEG_OPTS%
 
+set  video_common= -c:v wmv2  -s 480x270  -aspect 16:9
+set  audio_common= -c:a wmav2  -ac 2
+
+set  video_opts=%video_common%  -r 15 -q:v 25
+set  audio_opts=%audio_common%  -b:a 64k
+
 set  command_line=ffmpeg  -i "%source%"  ^
-  -c:v wmv2 -ac 2 -s 480x270 -b:a 64k -r 15 -q:v 25  ^
-  %FFMPEG_OPTS%  ^
-  "%output%"
+  %video_opts%  %audio_opts%  %FFMPEG_OPTS%  "%output%"
 ECHO  RunCmd: %command_line%
 
 @ECHO  ON
