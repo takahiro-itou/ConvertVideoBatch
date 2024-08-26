6c54b026f5fcc059a71fd02cadf4a24651bed1a5
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-26 20:21:36 +0900

    動画を編集するためのバッチファイルを実装

--- AsfEdit.bat
+++ AsfEdit.bat
@@ -9,25 +9,20 @@ setlocal
 IF "%~1" == ""  GOTO finish
 
 set  source=%~1
-set  output=%~n1.wmv
+set  output=%~2
 
 ECHO  Source : %source%
 ECHO  Output : %output%
 ECHO  Option : %FFMPEG_OPTS%
 
-set  video_common= -c:v wmv2  -s 480x270  -aspect 16:9
-set  audio_common= -c:a wmav2  -ac 2
-
-set  video_opts=%video_common%  -q:v 2
-set  audio_opts=%audio_common%  -b:a 320k
-
-set  command_line=ffmpeg  -i "%source%"  ^
-  %video_opts%  %audio_opts%  %FFMPEG_OPTS%  "%output%"
+set  command_line=ffmpeg  ^
+  -f concat  -safe 0  -i "%source%"  ^
+  -c:v copy -c:a copy -map 0:v -map 0:a  ^
+  "%output%"
 ECHO  RunCmd: %command_line%
 
 @ECHO  ON
 %command_line%
-FileTimeTools.exe  -r "%source%"  "%output%"
 @ECHO  OFF
 
 SHIFT
