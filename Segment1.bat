fcfb27df631133eb989506637fe1936c9f82424b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-28 21:12:49 +0900

    バッチファイルを新規作成
    (1) 動画から指定した区間を抽出する

---
+++ Segment1.bat
@@ -0,0 +1,35 @@
+
+@ECHO OFF
+setlocal
+
+@REM  CD /D %~dp0
+
+:loop
+
+IF "%~1" == ""  GOTO finish
+
+set  source=%~1
+set  time_s=%2
+set  time_e=%3
+set  output=%~4
+
+ECHO  Source : %source%
+ECHO  Output : %output%
+ECHO  Begin  : %time_s%
+ECHO  Last   : %time_e%
+ECHO  Option : %FFMPEG_OPTS%
+
+set  command_line=ffmpeg  ^
+  -i "%source%"  -ss %time_s%  -to %time_e%  ^
+  -c:v copy -c:a copy -async 1  ^
+  "%output%"
+ECHO  RunCmd: %command_line%
+
+@ECHO  ON
+%command_line%
+@ECHO  OFF
+
+SHIFT
+GOTO  loop
+
+:finish
