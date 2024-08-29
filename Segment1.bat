1c2658029b6f3b70c60290df028ccc7afd4a37ce
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-29 23:33:18 +0900

    区間抽出時のオプションを変更
    (1) -ss を入力オプションに変更

--- Segment1.bat
+++ Segment1.bat
@@ -20,9 +20,10 @@ ECHO  Length : %time_i%
 ECHO  Option : %FFMPEG_OPTS%
 
 set  copy_opts= -c:v copy -c:a copy  -async 1
+set  segment_opts=-ss %time_s%  -t %time_i%  -i "%source%"
 
 set  command_line=ffmpeg  ^
-  -ss %time_s%  -i "%source%"  -ss 0  -to %time_i%  ^
+  %segment_opts%  ^
   %copy_opts%  "%output%"
 ECHO  RunCmd: %command_line%
 
