6775cf78d352c4228ef8ca1e5046951ddd461bc9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-08-29 23:38:49 +0900

    リファクタリング

--- SegmentAlt1.bat
+++ SegmentAlt1.bat
@@ -10,19 +10,22 @@ IF "%~1" == ""  GOTO finish
 
 set  source=%~1
 set  time_s=%2
-set  time_e=%3
+set  time_i=%3
 set  output=%~4
 
 ECHO  Source : %source%
 ECHO  Output : %output%
 ECHO  Begin  : %time_s%
-ECHO  Last   : %time_e%
+ECHO  Length : %time_i%
 ECHO  Option : %FFMPEG_OPTS%
+
+set  interval= -ss %time_s%  -t %time_i%
+set  copy_opts= -c:v copy -c:a copy  -avoid_negative_ts make_zero
+set  segment_opts=%interval%  -accurate_seek  -i "%source%"
 
 set  command_line=ffmpeg  ^
   -i "%source%"  -ss %time_s%  -to %time_e%  ^
-  -c:v copy -c:a copy -async 1  ^
-  "%output%"
+  %copy_opts%  "%output%"
 ECHO  RunCmd: %command_line%
 
 @ECHO  ON
