f76577c1b1e138c6e78cd47f170c5b2fd5d9510e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-02-27 20:59:38 +0900

    不具合修正

--- Anime3.bat
+++ Anime3.bat
@@ -6,7 +6,7 @@ set  script_dir=%~dp0
 
 set  VIDEO_CONFIG= -r 24 -q:v 5
 set  AUDIO_CONFIG= -b:a 160k
-set  OTHER_CONFIG= -map 0:v:1 -map 0:a:1
+set  OTHER_CONFIG=
 
 
 CALL  "%script_dir%Common.bat"  %*
