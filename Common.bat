69d57fe853861c6669498662001d41214144c51a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-02-27 20:38:34 +0900

    ドライラン用のフラグを準備

--- Common.bat
+++ Common.bat
@@ -8,6 +8,7 @@ ECHO  Video Config : %VIDEO_CONFIG%
 ECHO  Audio Config : %AUDIO_CONFIG%
 ECHO  Other Config : %OTHER_CONFIG%
 ECHO  Extra Option : %FFMPEG_OPTS%
+ECHO  Flag Dry-Run : %FLAG_DRYRUN%
 
 :loop
 
@@ -30,10 +31,14 @@ set  command_line=ffmpeg  -i "%source%"  ^
   %video_opts%  %audio_opts%  %other_opts%  %FFMPEG_OPTS%  "%output%"
 ECHO  RunCmd: %command_line%
 
+IF "%FLAG_DRYRUN%" == "dryrun" GOTO dryrun
+
 @ECHO  ON
 %command_line%
 FileTimeTools.exe  -C  -r "%source%"  "%output%"
 @ECHO  OFF
+
+:dryrun
 
 SHIFT
 GOTO  loop
