a6ad5c963754bbc5a9bf171df460c5bf9efe3453
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-02-28 23:25:49 +0900

    リファクタリング

--- ShogiIgoTaikyoku.bat
+++ ShogiIgoTaikyoku.bat
@@ -2,35 +2,11 @@
 @ECHO OFF
 setlocal
 
-@REM  CD /D %~dp0
+set  script_dir=%~dp0
 
-:loop
+set  VIDEO_CONFIG= -r 5 -q:v 11
+set  AUDIO_CONFIG= -b:a 160k
+set  OTHER_CONFIG=
 
-IF "%~1" == ""  GOTO finish
 
-set  source=%~1
-set  output=%~n1.wmv
-
-ECHO  Source : %source%
-ECHO  Output : %output%
-ECHO  Option : %FFMPEG_OPTS%
-
-set  video_common= -c:v wmv2  -s 480x270  -aspect 16:9
-set  audio_common= -c:a wmav2  -ac 2  -af volume=6dB
-
-set  video_opts=%video_common%  -r 5 -q:v 11
-set  audio_opts=%audio_common%  -b:a 160k
-
-set  command_line=ffmpeg  -i "%source%"  ^
-  %video_opts%  %audio_opts%  %FFMPEG_OPTS%  "%output%"
-ECHO  RunCmd: %command_line%
-
-@ECHO  ON
-%command_line%
-FileTimeTools.exe  -C  -r "%source%"  "%output%"
-@ECHO  OFF
-
-SHIFT
-GOTO  loop
-
-:finish
+CALL  "%script_dir%Common.bat"  %*
