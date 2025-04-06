366c616d7fb133fb23be761a69b1e8dc67ba2313
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-06 13:00:36 +0900

    不具合修正

--- WriteScript.bat
+++ WriteScript.bat
@@ -13,10 +13,10 @@ IF "%~1" == ""  GOTO finish
 
 set  source=%~1
 
-set  conv_cmd=%convtype%  "%source%"
+set  conv_cmd=CALL  %convtype%  "%source%"
 set  conv_log=cscript //NoLogo Tee.vbs  -a Convert.log
 
-ECHO  %conv_cmd%  ^^^|  %conv_log%  | cscript //NoLogo  Tee.vbs -a  Run.bat
+ECHO %conv_cmd%  ^^^|  %conv_log%  | cscript //NoLogo  Tee.vbs -a  Run.bat
 
 SHIFT
 GOTO  loop
