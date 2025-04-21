74354a9bf246cc66eb9ee6d0c449a561a06a9ee3
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-21 20:01:23 +0900

    抽出するストリームを指定できるバージョンを実装

---
+++ Variety1-Spec.bat
@@ -0,0 +1,15 @@
+
+@ECHO OFF
+setlocal
+
+set  script_dir=%~dp0
+
+set  VIDEO_CONFIG= -r 15 -q:v 5
+set  AUDIO_CONFIG= -b:a 192k
+set  OTHER_CONFIG= -map 0:v:%1 -map 0:a:%2
+
+SHIFT
+SHIFT
+
+
+CALL  "%script_dir%Common.bat"  %*
