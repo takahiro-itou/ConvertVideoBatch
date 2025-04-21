d5f0d84185e7a44c19b0662a4bde549345730bb4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-21 20:00:19 +0900

    不具合修正

--- Anime4-Spec.bat
+++ Anime4-Spec.bat
@@ -6,7 +6,7 @@ set  script_dir=%~dp0
 
 set  VIDEO_CONFIG= -r 15 -q:v 7
 set  AUDIO_CONFIG= -b:a 128k
-set  OTHER_CONFIG= -map 0:v:%1-map 0:a:%2
+set  OTHER_CONFIG= -map 0:v:%1 -map 0:a:%2
 
 SHIFT
 SHIFT
--- ShogiIgoTaikyoku-Spec.bat
+++ ShogiIgoTaikyoku-Spec.bat
@@ -6,7 +6,7 @@ set  script_dir=%~dp0
 
 set  VIDEO_CONFIG= -r 5 -q:v 11
 set  AUDIO_CONFIG= -b:a 160k
-set  OTHER_CONFIG= -map 0:v:%1-map 0:a:%2
+set  OTHER_CONFIG= -map 0:v:%1 -map 0:a:%2
 
 SHIFT
 SHIFT
