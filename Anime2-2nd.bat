d6ab0b97cc0574eb4622a0c161cbcd22352daf14
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-26 15:15:42 +0900

    リファクタリング

--- Anime2-2nd.bat
+++ Anime2-2nd.bat
@@ -4,9 +4,8 @@ setlocal
 
 set  script_dir=%~dp0
 
-set  VIDEO_CONFIG= -r 30000/1001 -q:v 3
-set  AUDIO_CONFIG= -b:a 192k
-set  OTHER_CONFIG= -map 0:v:1 -map 0:a:1
+CALL  "%script_dir%Config\Anime1.cnf.bat"
 
+set  MAP_CONFIG= -map 0:v:1 -map 0:a:1
 
 CALL  "%script_dir%Common.bat"  %*
--- Anime2.bat
+++ Anime2.bat
@@ -4,9 +4,8 @@ setlocal
 
 set  script_dir=%~dp0
 
-set  VIDEO_CONFIG= -r 30000/1001 -q:v 3
-set  AUDIO_CONFIG= -b:a 192k
-set  OTHER_CONFIG=
+CALL  "%script_dir%Config\Anime1.cnf.bat"
 
+set  MAP_CONFIG=
 
 CALL  "%script_dir%Common.bat"  %*
