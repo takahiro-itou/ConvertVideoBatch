4c6928881c00704c0dc735ccfda154d5a28b60ff
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-26 15:00:54 +0900

    リファクタリング

--- Anime1.bat
+++ Anime1.bat
@@ -4,9 +4,8 @@ setlocal
 
 set  script_dir=%~dp0
 
-set  VIDEO_CONFIG= -q:v 2
-set  AUDIO_CONFIG= -b:a 320k
-set  OTHER_CONFIG=
+CALL  "%script_dir%Config\Anime1.cnf.bat"
 
+set  MAP_CONFIG=
 
 CALL  "%script_dir%Common.bat"  %*
