5de26ae513002dabb38887be7c804d390bad1daf
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-04-26 15:21:09 +0900

    リファクタリング

--- Anime3-Spec.bat
+++ Anime3-Spec.bat
@@ -4,12 +4,8 @@ setlocal
 
 set  script_dir=%~dp0
 
-set  VIDEO_CONFIG= -r 24 -q:v 5
-set  AUDIO_CONFIG= -b:a 160k
-set  OTHER_CONFIG= -map 0:v:%1 -map 0:a:%2
+CALL  "%script_dir%Config\Anime3.cnf.bat"
 
-SHIFT
-SHIFT
+set  MAP_CONFIG= -map 0:v:%1 -map 0:a:%2
 
-
-CALL  "%script_dir%Common.bat"  %*
+CALL  "%script_dir%Common-Spec.bat"  %*
